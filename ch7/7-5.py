796094e50d9f2bc3f0547e7e21d3608b2c322959
POL6463 <kevinkim0617kim@gmail.com>
2022-03-07 13:59:58 +0900

    7-5

--- ch7/7-5.py
+++ ch7/7-5.py
@@ -1,6 +1,7 @@
 def binary_search(array, target, start, end):
     while start <= end:
-        mid = (start + end) //2
+        mid = (start + end) // 2
+
         if array[mid] == target:
             return mid
         elif array[mid] > target:
@@ -13,13 +14,12 @@ def binary_search(array, target, start, end):
 n = int(input())
 array = list(map(int, input().split()))
 array.sort()
-
 m = int(input())
 x = list(map(int, input().split()))
 
 for i in x:
     result = binary_search(array, i, 0, n-1)
-    if result != None:
-        print('yess', end=' ')
+    if result is not None:
+        print('yes', end=' ')
     else:
         print('no', end=' ')
