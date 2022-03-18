75580a8ea28f4dc1ecb2d4542c235634ecc038c3
POL6463 <kevinkim0617kim@gmail.com>
2022-03-18 23:02:00 +0900

    asf

--- ch9/9-3.py
+++ ch9/9-3.py
@@ -21,7 +21,7 @@ for k in range(1, n + 1):
 for a in range(1, n + 1):
     for b in range(1, n + 1):
         if graph[a][b] == INF:
-            print("INFasdf", end=" ")
+            print("INF", end=" ")
         else:
             print(graph[a][b], end=" ")
     print()
